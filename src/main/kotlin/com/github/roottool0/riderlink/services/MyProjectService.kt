fb2039a4613c1ceb5395de3e3004556ad9d07b79
GitHub Action <action@github.com>
2026-09-19 16:15:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/roottool0/riderlink/services/MyProjectService.kt
+++ src/main/kotlin/com/github/roottool0/riderlink/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.roottool0.riderlink.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.roottool0.riderlink.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/roottool0/riderlink/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/roottool0/riderlink/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.roottool0.riderlink.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
